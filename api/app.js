edfe1906746a81e9b1ae5118e4a8197f1695c973
es97 <estellawang0218@gmail.com>
2019-07-16 02:57:12 -0700

    good

--- api/app.js
+++ api/app.js
@@ -40,6 +40,10 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
---
+++ client/build/precache-manifest.556285e45400e0b7affc643e230b114e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f40bcda3885a048a7c664ba1a13f25",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "86aeb7ed3cfe2887b960",
+    "url": "/myapp/static/css/main.5135f8a7.chunk.css"
+  },
+  {
+    "revision": "03aef7d10c22f26a0607",
+    "url": "/myapp/static/js/2.36c4a3de.chunk.js"
+  },
+  {
+    "revision": "86aeb7ed3cfe2887b960",
+    "url": "/myapp/static/js/main.7a7960fe.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);

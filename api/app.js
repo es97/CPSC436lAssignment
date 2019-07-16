0b6758d7f490390a9407abafc7b9a135ee0ec5e2
es97 <estellawang0218@gmail.com>
2019-07-16 11:31:16 -0700

    good

--- api/app.js
+++ api/app.js
@@ -32,14 +32,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build'));
-})
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'client/build'));
+// })
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

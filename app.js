f26371df1636243098d2d9b1d07287ed5473f42b
es97 <estellawang0218@gmail.com>
2019-07-16 11:58:47 -0700

    final

--- app.js
+++ app.js
@@ -31,12 +31,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
